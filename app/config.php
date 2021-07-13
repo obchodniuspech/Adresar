962651112c751849291e49f3adf6a8a60b8612cd
obchodniuspech <pesatmichal@gmail.com>
2021-07-13 22:07:19 +0200

    Development

--- app/config.php
+++ app/config.php
@@ -1,3 +1,12 @@
 <?php
 
+// Default configs
 define("HOMEDIR", "/Users/michalpesat/Documents/GitHub/adresar_blueghost/Adresar_BlueGhost/");
+
+// Elastic Search
+define("ELASTIC", false);
+
+// MySQL credentials
+define("DB_SERVER", "localhost");
+define("DB_USER", "localhost");
+define("DB_PASS", "localhost");
--- readme.md
+++ readme.md
@@ -1,4 +1,11 @@
 Todos 
 
+HTML / CSS
 https://getbootstrap.com/docs/5.0/examples/sidebars/
 - stahnout na local vsechny vnorene soubory
+
+PHP
+- API na spravu udaju
+
+Ostatni
+- moznost Elastic Search
